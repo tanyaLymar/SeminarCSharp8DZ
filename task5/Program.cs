8a60ad69b66da25a427a2118f21a1c5a1a404932
Tatiana Lymar <tanya.lymar87@gmail.com>
2023-02-21 21:19:08 +0300

    Task5 solved

---
+++ task5/Program.cs
@@ -0,0 +1,26 @@
+﻿//Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+Console.Write("Введите количество строк: ");
+
+
+int n = int.Parse(Console.ReadLine() ?? "");
+
+
+for (int y = 0; y < n; y++)
+{
+    int c = 1;
+    for (int q = 0; q < n - y; q++)
+    {
+        Console.Write("   ");
+    }
+
+        for (int x = 0; x <= y; x++)
+            {
+                Console.Write("  {0}  ", c);
+                c = c * (y - x) / (x + 1);
+
+            }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.WriteLine();
